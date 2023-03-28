8e4a27f049a49de6a0c108aa4806a28410ed75a5
HyeonJae <81461486+la1av1a@users.noreply.github.com>
2023-03-28 16:36:23 +0900

    Revert "Devdev"

--- src/main/java/com/smu/smuenip/Infrastructure/config/exception/UnAuthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.smu.smuenip.Infrastructure.config.exception;
-
-
-public class UnAuthorizedException extends RuntimeException {
-    
-    public UnAuthorizedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/smu/smuenip/enums/Provider.java
+++
@@ -1,9 +0,0 @@
-package com.smu.smuenip.enums;
-
-import lombok.Getter;
-
-@Getter
-public enum Provider {
-
-    KAKAO, GOOGLE, LOCAL
-}
